2cd3d93d96dafb25b9261bb18ec2d9516c151d69
Christophe <christophe@colorfield.eu>
2017-10-04 22:32:21 +0200

    Add carousel template, countdown library, Twitter follow to maintenance page

--- web/modules/custom/planet_drupal/src/Controller/OnboardController.php
+++ web/modules/custom/planet_drupal/src/Controller/OnboardController.php
@@ -6,7 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\entity_tools\EntityService;
+use Drupal\entity_tools\EntityTools;
 
 /**
  * Class OnboardController.
@@ -16,40 +16,48 @@ class OnboardController extends ControllerBase {
   /**
    * Drupal\entity_tools\EntityService definition.
    *
-   * @var \Drupal\entity_tools\EntityService
+   * @var \Drupal\entity_tools\EntityTools
    */
-  protected $entityToolsEntity;
+  protected $entityService;
 
   /**
    * Constructs a new OnboardController object.
    */
-  public function __construct(EntityService $entity_tools_entity) {
-    $this->entityToolsEntity = $entity_tools_entity;
+  public function __construct(EntityTools $entity_tools_entity) {
+    $this->entityService = $entity_tools_entity;
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('entity_tools.entity'));
+    return new static($container->get('entity_tools'));
   }
 
   /**
    * Index.
    *
    * @return string
-   *   Return Hello string.
+   *   Return Onboard render array.
    */
   public function index() {
+
+    $workNodes = $this->entityService->getNodes('work');
+    $workCarouselItems = [];
+    foreach ($workNodes as $node) {
+      $workCarouselItems[] = $this->entityService->getEntityView($node, 'teaser'); // @todo change by slide
+    }
+
     $text = $this->t('Project roadmap');
     $options = ['absolute' => TRUE];
+    // @todo get from configuration
     $url = Url::fromRoute('entity.node.canonical', ['node' => 31], $options);
     $link = Link::fromTextAndUrl($text, $url)->toString();
     drupal_set_message($this->t('This site is a work in progress, view the @project_roadmap', ['@project_roadmap' => $link]), 'warning');
     $build['onboard_page'] = [
       '#theme' => 'onboard',
+      '#work_carousel_items' => $workCarouselItems,
     ];
-
 
     return $build;
   }
