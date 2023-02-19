b3d32791894f0892eb161c4d578bfe75c25ff3ef
Christophe Jossart <christophe@colorfield.eu>
2023-02-19 16:31:46 +0100

    chore: prettier, eslint, next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  staticImage: true,
+  latex: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true
 })
 
 module.exports = withNextra()
