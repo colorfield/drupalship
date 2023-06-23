ea01f0ffc719a10e53b18fbc4a5b0e4b3a37d627
Christophe Jossart <christophe.jossart@amazeelabs.com>
2023-06-23 08:19:41 +0200

    feat: add matomo

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { init } from "@socialgouv/matomo-next";
+import type { AppProps } from 'next/app'
+import 'nextra-theme-docs/style.css'
+
+function DrupalshipApp({ Component, pageProps }: AppProps) {
+
+  const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || ''
+  const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || ''
+
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+
+  return <Component {...pageProps} />
+}
+
+export default DrupalshipApp
