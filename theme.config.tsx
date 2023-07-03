09cd4fc0c458b71bf88b78e7e4cb3bc074ab164a
Christophe Jossart <christophe.jossart@amazeelabs.com>
2023-07-03 08:01:57 +0200

    chore: footer links display

--- theme.config.tsx
+++ theme.config.tsx
@@ -51,13 +51,11 @@ const config: DocsThemeConfig = {
   footer: {
     text: ( 
       <>
-        <ul>
-          <li><a href="/about">About</a></li>
-          <li><a href="/roadmap">Roadmap</a></li>
-          <li><a href="/credits">Credits</a></li>
-          <li><a href="https://drupal.community/@drupalship" target="_blank" rel="me">Mastodon ↗</a></li>
-          <li><a href="https://twitter.com/drupalship" target="_blank" rel="noreferrer">Twitter ↗</a></li>
-        </ul>
+        <a href="/about" className='mb-6'>About</a>&nbsp;|&nbsp; 
+        <a href="/roadmap">Roadmap</a>&nbsp;|&nbsp; 
+        <a href="/credits">Credits</a>&nbsp;|&nbsp; 
+        <a href="https://drupal.community/@drupalship" target="_blank" rel="me">Mastodon ↗</a>&nbsp;|&nbsp; 
+        <a href="https://twitter.com/drupalship" target="_blank" rel="noreferrer">Twitter ↗</a>
       </>
     ), 
   },
