86134284d071008d2250fcef129a46c50d309196
Christophe Jossart <christophe.jossart@amazeelabs.com>
2024-03-07 09:56:26 +0100

    chore: remove matomo, bump nextra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
-import { useEffect } from 'react'
-import { init } from '@socialgouv/matomo-next'
 import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
 import 'nextra-theme-docs/style.css'
 
 function DrupalshipApp({ Component, pageProps }: AppProps) {
-
-  const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || ''
-  const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || ''
-
-  useEffect(() => {
-    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
-  }, []);
-
   return (
     <PlausibleProvider domain='drupalship.org' customDomain='https://plausible.colorfield.dev'>
       <Component {...pageProps} />
